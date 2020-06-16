07bf76b2cd9bf76cfc7130df5ca158f7a42a424b
Austin Kennedy <kennedyaustin06561@gmail.com>
2020-06-16 19:20:54 -0400

    added a function that will hopefully show _ or the letter if it is part of the word, also tested to see if Letter constructor works, works

--- Letter.js
+++ Letter.js
@@ -1,6 +1,8 @@
 const Letter= function (letter) {
 
+    // Letter input by user
     this.letter= letter,
+    // Tells whether or not the user has made a guess
     this.guessed= false,
     // The function below will look for user input and when they do guess a letter
     // the boolean value of the 'guessed' will be true
@@ -13,7 +15,19 @@ const Letter= function (letter) {
         }
 
     }
+    this.displayLetter= function() {
+
+        if (!this.guessed) {
+            return '_'
+        } else {
+            return this.letter
+        }
+
+    }
 
 }
+
+const blue= new Letter('a')
+console.log(blue)
 
 module.exports= Letter
