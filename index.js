06b0bec9dfea9df82c8fba57b53191191f0f407b
Austin Kennedy <kennedyaustin06561@gmail.com>
2020-06-16 22:40:12 -0400

    started adding the restrictions on user input to a-z, excluding tab and such

--- index.js
+++ index.js
@@ -5,25 +5,56 @@ var inquirer= require('inquirer')
 // My array of words to choose from
 const myArrayForNow= ['boom','cat']
 
-// These three lines will choose a random word for the user to guess form the array above
-let randomWordGen= Math.floor(Math.random() * myArrayForNow.length)
-let randomWordChosen= myArrayForNow[randomWordGen]
-let chosenWord= new Word(randomWordChosen)
-
 // These two empty arrays will hold the letters that the user inputs
 let correctGuessedLetters= []
 let incorrectGuessedLetters= []
-// This will be used to help denote the end of the game
+// This will be used to help denote the end of the startGame
 let requireWordEnd= false
+let userGuessesLeft= 10
+let usableCharacters= /[a-zA-Z]/
 
-function game() {
+function startGame() {
 
+    while (requireWordEnd) {
 
+        // These three lines will choose a random word for the user to guess form the array above
+        let randomWordGen= Math.floor(Math.random() * myArrayForNow.length)
+        let randomWordChosen= myArrayForNow[randomWordGen]
+        let chosenWord= new Word(randomWordChosen)
 
+    }
+    let usedWords= []
+    
+    if (usedWords.includes(false)) {
 
-}
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    message: 'Guess a letter between A and Z!',
+                    name: 'userInput'
+                }
+            ])
+            .then(answers => {
+                if (!usableCharacters.includes(answers.userInput) || answers.userInput.length > 1) {
 
-// This function will be put inside of the game function for when the user loses the game,
+                    console.log('\nYou have entered an invalid character, please try again!\n')
+                    startGame()
+
+                } else if (incorrectGuessedLetters.includes(answers.userInput) 
+                           || correctGuessedLetters.includes(answers.userInput) 
+                           || answers.userInput === '') {
+
+                            console.log('\nYou have either not guessed anything, or nothing was guessed at all!\n')
+                            startGame()
+
+                }
+
+            })
+
+    }
+
+// This function will be put inside of the startGame function for when the user loses the startGame,
 // or when the user is able to guess all of the words correctly
 function restartGame() {
 
@@ -53,4 +84,4 @@ function restartGame() {
 
 }
 
-game()
+startGame()
