16cbfafa7b085e05f4970e32bccb4e5be9b65e45
Austin Kennedy <kennedyaustin06561@gmail.com>
2020-06-16 20:15:51 -0400

    starting on the index.js, added the require tag to the word.js file and the inquirer npm

---
+++ index.js
@@ -0,0 +1,7 @@
+// Requiring the files/ npm files that are needed to run the file
+const Word= require('./Word')
+const inquirer= require('inquirer')
+
+// My array of words to choose from
+const myArrayForNow= ['boom','cat']
+
